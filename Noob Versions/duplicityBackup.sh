eb148e135d1b5f9f0e79965730fc7c6437b27ccb
Sylvester Black <Nope.test.dev>
2021-04-01 15:59:18 +0200

    Fixed Some Errors And made it more Ease Of Acces

--- Noob Versions/duplicityBackup.sh
+++ Noob Versions/duplicityBackup.sh
@@ -5,12 +5,16 @@ GNU nano 4.8                                                                    
 B2_ACCOUNT="App KeyID / Master KeyID"
 B2_KEY="ApplicationKEY / MasterKEY"
 B2_BUCKET="BucketName"
+B2_DIR="Name of folder in Cloud"
+## Example Below Becomes YYYY-MM-DD The extra variable is needed to calculate time then echo it
+#now=$(date +%F)
+#B2_DIR="$now"
 
 # Local directory to backup
 LOCAL_DIR="/home/databasedumps"
 
 # The Command That does the magick
-duplicity ${LOCAL_DIR} b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}
+duplicity ${LOCAL_DIR} b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
 
 unset B2_ACCOUNT
 unset B2_KEY
--- Noob Versions/duplicityRestore.sh
+++ Noob Versions/duplicityRestore.sh
@@ -5,12 +5,13 @@ GNU nano 4.8                                                                    
 B2_ACCOUNT="App KeyID / Master KeyID"
 B2_KEY="ApplicationKEY / MasterKEY"
 B2_BUCKET="BucketName"
+B2_DIR="Name of the Folder in Cloud to Be Restored"
 
 # Local directory to Restore
 LOCAL_DIR="/home/databasedumps"
 
 # The Command that does the Magick
-duplicity --force restore  b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET} ${LOCAL_DIR}
+duplicity --force restore --file-to-restore ${B2_DIR} b2://${B2_ACCOUNT}:${B2_KEY}@${B2_BUCKET} ${LOCAL_DIR}
 
 unset B2_ACCOUNT
 unset B2_KEY
